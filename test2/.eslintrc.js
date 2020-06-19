e9f138dd6ae1e0fc9bae8344ad891516f2970d07
Ethan Brown <z5162332@student.unsw.edu.au>
2020-06-19 21:21:15 +1000

    Updating packages and fixing vulnerabilities; ignoring dist

--- test2/.eslintrc.js
+++ test2/.eslintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
-  "plugins": [ "react" ],
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "parser": "babel-eslint"
+    "plugins": ["react"],
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parser": "babel-eslint",
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    }
 };
-
