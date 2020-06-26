720e57aadbe9b1d6d4d2cbb9978091a5aae07cb1
rui.tong <ruiace@DESKTOP-5CNR0D3.localdomain>
2020-06-26 21:48:03 +1000

    added draw function and finalised freqvisualiser

--- app/src/js/editor/FreqVisualiser.jsx
+++ app/src/js/editor/FreqVisualiser.jsx
@@ -24,6 +24,24 @@ export default class FreqVisualiser extends React.Component {
         this.draw();
     }
 
+    draw() {
+        requestAnimationFrame(this.draw);
+
+        const bufferLength = this.state.analyser.frequencyBinCount;
+        this.state.analyser.getByteTimeDomainData(this.dataArray);
+        this.canvasCtx.fillStyle = 'black';
+        this.canvasCtx.fillRect(0, 0, this.state.width, this.state.height);
+        this.canvasCtx.beginPath();
+
+        const barWidth = (this.state.width / bufferLength) * 2.5;
+            
+        for(let i = 0, x = 0; i < bufferLength; i++, x += barWidth + 1) {
+            const barHeight = this.dataArray[i] / 2;
+            this.canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+            this.canvasCtx.fillRect(x, this.state.height - barHeight / 2, barWidth, barHeight);
+        }
+    }
+
     render() {
         return (
             <div className="col-3">
