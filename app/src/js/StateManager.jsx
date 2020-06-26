5effa7954000076478eefa381dcfb0b1e86a610a
Madhav <padmakma@hotmail.com>
2020-06-26 21:52:54 +1000

    Added a state manager that

---
+++ app/src/js/StateManager.jsx
@@ -0,0 +1,10 @@
+export default class StateManager {
+    constructor() {
+        this.state = {};
+    }
+
+    setState(obj) {
+        for (let key in obj)
+            this.state[key] = obj[key];
+    }
+}
