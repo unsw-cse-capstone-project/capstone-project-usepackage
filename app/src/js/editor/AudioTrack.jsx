99c8b137c8acae5e12a2c01d9c9a950c94eea271
Madhav <padmakma@hotmail.com>
2020-07-03 16:17:46 +1000

    Implemented slicing and cutting in AudioTrack, so files can be broken into sections

--- app/src/js/editor/AudioTrack.jsx
+++ app/src/js/editor/AudioTrack.jsx
@@ -1,5 +1,9 @@
 import createSoundTouchNode from '../soundtouch/createSoundTouchNode.js';
 import WavAudioEncoder from '../soundtouch/WavAudioEncoder.js'
+import PromiseQueue from '../PromiseQueue.jsx';
+
+const lamejs = window.lamejs;
+const Encoder = window.Encoder;
 
 export default class AudioTrack {
     constructor (fileData, data) {
@@ -11,6 +15,7 @@ export default class AudioTrack {
         this.split = this.split.bind(this);
         this.copy = this.copy.bind(this);
         this.play = this.play.bind(this);
+        this.playFrom = this.playFrom.bind(this);
         this.setGain = this.setGain.bind(this);
         this.processData = this.processData.bind(this);
         this.playback = this.playback.bind(this);
@@ -20,8 +25,14 @@ export default class AudioTrack {
         this.processData(data);
         this.wipeData = this.wipeData.bind(this);
         this.record = this.record.bind(this);
-        window.record = this.record;
+        window.play = this.playFrom;
+        this.attachAnalyser = null;
+        this.currentCut = 0;
         //this.cut(this.buffer.sampleRate * 3);
+    }
+
+    setAnalyserCallback(cb) {
+        this.attachAnalyser = cb;
     }
 
     //track contents
@@ -31,31 +42,29 @@ export default class AudioTrack {
 
     // Convert a length of the audioBuffer into a separate track?
     toBuffer(track) {
+        const pitch = this.controller.pitchVal;
         const tempo = this.controller.tempoVal;
-        const rate = this.buffer.sampleRate;
+        const rate = this.fileData.audioBuffer.sampleRate;
+        console.log("Length", track.length);
+        console.log("Context size", Math.ceil(track.length / tempo));
         const context = new OfflineAudioContext(2, Math.ceil(track.length / tempo), rate);
         return context.audioWorklet.addModule('/soundtouch/soundtouch-worklet.js').then(() => {
-            let buffer = this.buffer;
-            if (tempo < 1) {
-                buffer = context.createBuffer(2, Math.ceil(track.length / tempo), rate);
-                buffer.copyToChannel(this.buffer.getChannelData(0), 0);
-                buffer.copyToChannel(this.buffer.getChannelData(1), 1);
-            }
+            const buffer = context.createBuffer(2, track.length, rate);
+            buffer.copyToChannel(this.fileData.audioBuffer.getChannelData(0).slice(track.time), 0);
+            buffer.copyToChannel(this.fileData.audioBuffer.getChannelData(1).slice(track.time), 1);
             const soundtouch = createSoundTouchNode(context, AudioWorkletNode, buffer);
             const graph = this.copy(track.graph, context);
             return new Promise((resolve) => {
                 soundtouch.on('initialized', () => {
                     soundtouch.tempo = tempo;
+                    soundtouch.pitch = pitch;
                     const buff = soundtouch.connectToBuffer();
                     soundtouch.connect(graph.splitter);
                     graph.merger.connect(context.destination);
-                    buff.start(0, track.time / rate, (track.length / tempo) / rate);
+                    buff.start();
                     context.startRendering().then((buff) => {
-                        const encode = new WavAudioEncoder(rate, 2);
-                        encode.encode([buff.getChannelData(0), buff.getChannelData(1)]);
-                        const blob = encode.finish();
-                        console.log(blob);
-                        resolve(blob);
+                        console.log("End length", buff.length);
+                        resolve(buff);
                     });
                 });
             })
@@ -63,44 +72,28 @@ export default class AudioTrack {
     }
 
     record() {
-        const promises = [];
+        const queue = new PromiseQueue();
         this.cuts.forEach((track) => {
-            promises.push((arr) => {
+            queue.add((arr) => {
                 return this.toBuffer(track).then((data) => {
-                    console.log([...arr, data]);
+                    console.log([...arr, data.getChannelData(0), data.getChannelData(1)]);
                     return [...arr, data];
                 });
             });
         });
-        return promises.reduce((prev, curr) => {
-            return prev.then(curr);
-        }, Promise.resolve([]));
-    // record() {
-    //     const file = this.files[0].offline;
-    //     file.soundtouch.connect(file.splitter);
-    //     const buff = file.soundtouch.connectToBuffer();
-    //     //const recorder = new Recorder(buff);
-    //     //recorder.record();
-    //     buff.start();
-    //     file.context.startRendering().then((buff) => {
-    //         console.log(buff);
-    //         const encode = new WavAudioEncoder(44100, 2);
-    //         encode.encode([buff.getChannelData(0), buff.getChannelData(1)]);
-    //         const blob = encode.finish();
-    //         console.log(buff.getChannelData(0));
-    //         console.log(blob);
-
-    //         //recorder.exportWAV((e) => {
-    //         //     console.log(e);
-    //         //     console.log(e.arrayBuffer());
-    //             let a = document.createElement('a');
-    //             a.download = 'test.wav';
-    //             a.href = URL.createObjectURL(blob);
-    //             a.innerText = "succ"
-    //             document.body.appendChild(a);
-    //         // })
-    //     });
-    // }
+        return queue.resolve([]).then((buffs) => {
+            const size = buffs.reduce((prev, next) => {
+                return prev + next.length;
+            }, 0);
+            const buffer = this.context.createBuffer(2, size, this.fileData.audioBuffer.sampleRate);
+            let offset = 0;
+            buffs.forEach((buff) => {
+                for (let i = 0; i < 2; i++)
+                    buffer.copyToChannel(buff.getChannelData(i), i, offset);
+                offset += buff.length;
+            });
+            return buffer;
+        });
     }
 
     getTime() {
@@ -110,6 +103,18 @@ export default class AudioTrack {
         //     currTime = detail.formattedTimePlayed
         // })
         return 
+    }
+
+    get CurrentCut() {
+        return this.currentCut
+    }
+
+    get rate() {
+        return this.buffer.sampleRate;
+    }
+
+    set CurrentCut(val) {
+        this.currentCut = val
     }
 
     wipeData() {
@@ -123,8 +128,11 @@ export default class AudioTrack {
     }
 
     setGain(value, channel) {
+        this.cuts[this.currentCut].graph.gain[channel].gain.value = value;
+    }
 
-        this.cuts[0].graph.gain[channel].gain.value = value;
+    getGain(channel) {
+        return this.cuts[this.currentCut].graph.gain[channel].gain.value;
     }
 
     playback(value) {
@@ -135,15 +143,52 @@ export default class AudioTrack {
         this.controller.pitch = value;
     }
 
-    play() {
+    playFrom(time) {
+        time = Math.floor(time * this.buffer.sampleRate);
+        const tempo = this.controller.tempoVal;
+        const pitch = this.controller.pitchVal;
+        if (this.inprogress) {
+            this.controller.stop();
+            this.controller.cutCallback(null);
+            this.controller.disconnect();
+            this.controller.disconnectFromBuffer();
+            this.controller.off('play');
+            this.inprogress = false;
+        }
+        this.wipeData();
+        this.buffer = this.fileData.audioBuffer;
+        const newbuff = this.context.createBuffer(2, this.buffer.length - time, this.buffer.sampleRate);
+        newbuff.copyToChannel(this.buffer.getChannelData(0).slice(time), 0);
+        newbuff.copyToChannel(this.buffer.getChannelData(1).slice(time), 1);
+        const newdata = {...this.fileData,
+            audioBuffer: newbuff
+        };
+        AudioTrack.newNode(newdata).then((data) => {
+            this.processData(data);
+            this.controller.tempo = tempo;
+            this.controller.pitch = pitch;
+            this.cuts.forEach((cut) => {
+                cut.graph = this.copy(cut.graph);
+            });
+            this.play(time);
+        });
+    }
+
+    
+
+    play(time = 0) {
         const controllerNode = this.controller;
         if (this.inprogress) {
             controllerNode.play();
             return;
         }
-        const calls = this.cuts.map((cut, index) => {
+        const calls = this.cuts.filter((cut) => {
+            return cut.time + cut.length > time;
+        }).map((cut, index) => {
             return {
+                cut: cut,
                 callback: (time) => {
+                    console.log("TRANSITION MADE", time);
                     controllerNode.disconnect();
                     cut.graph.merger.disconnect();
                     if (calls.length == 0) {
@@ -152,24 +197,29 @@ export default class AudioTrack {
                     }
                     controllerNode.connect(this.cuts[index + 1].graph.splitter);
                     this.cuts[index + 1].graph.merger.connect(this.context.destination);
+                    if (this.attachAnalyser)
+                        this.attachAnalyser(this.cuts[index + 1].graph.analyser);
                     const call = calls.shift();
                     controllerNode.makeCallback(call.time).then(call.callback);
                     controllerNode.cutCallback(() => {
                         this.cuts[index + 1].graph.merger.disconnect();
                     });
                 },
-                time: cut.time + cut.length
+                time: cut.time + cut.length - time
             }
         });
         const call = calls.shift();
         controllerNode.makeCallback(call.time).then(call.callback);
         controllerNode.cutCallback(() => {
-            this.cuts[0].graph.merger.disconnect();
+            call.cut.graph.merger.disconnect();
         });
-        this.cuts[0].graph.merger.connect(this.context.destination);
-        controllerNode.connectToBuffer();
-        controllerNode.connect(this.cuts[0].graph.splitter);
+        call.cut.graph.merger.connect(this.context.destination);
+        const buff = controllerNode.connectToBuffer();
+        if (this.attachAnalyser)
+            this.attachAnalyser(call.cut.graph.analyser);
+        controllerNode.connect(call.cut.graph.splitter);
         controllerNode.play();
+        buff.start(0, time / this.buffer.sampleRate);
         this.inprogress = true;
     }
 
@@ -178,6 +228,8 @@ export default class AudioTrack {
     }
 
     stop() {
+        const tempo = this.controller.tempoVal;
+        const pitch = this.controller.pitchVal;
         this.controller.stop();
         this.controller.cutCallback(null);
         this.controller.disconnect();
@@ -187,9 +239,12 @@ export default class AudioTrack {
         this.wipeData();
         AudioTrack.newNode(this.fileData).then((data) => {
             this.processData(data);
+            this.controller = data.controller;
+            this.controller.tempo = tempo;
+            this.controller.pitch = pitch;
             this.cuts.forEach((cut) => {
                 cut.graph = this.copy(cut.graph);
-            })
+            });
         });
     }
 
@@ -236,9 +291,6 @@ export default class AudioTrack {
     getAnalyser(i) {
         return this.cuts[0].graph.analyser[i];
     }
-
-    
-
 }
 
 AudioTrack.newNode = (fileData) => {
